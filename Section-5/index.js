0d3a7b4fdaf920d003c97598d47c44978223092b
bahategar <bahategar@outlook.com>
2023-12-12 20:21:21 +0700

    Update index.js

---
+++ Section-5/index.js
@@ -0,0 +1,11 @@
+const elToggle = document.querySelector(".sidebar-toggle");
+const elClose = document.querySelector(".close-btn");
+
+elToggle.addEventListener("click", function() {
+    let elSidebar = document.querySelector(".sidebar");
+    elSidebar.classList.toggle("show-sidebar");
+});
+elClose.addEventListener("click", function() {
+    let elSidebar = document.querySelector(".sidebar");
+    elSidebar.classList.remove("show-sidebar");
+})
